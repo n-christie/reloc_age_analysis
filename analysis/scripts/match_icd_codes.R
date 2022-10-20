ee7a66b11d6ddfc27824eaf7739e823a97fcb075
Nick Christie <30588830+n-christie@users.noreply.github.com>
2022-10-20 08:39:56 +0200

    updated linking table

--- analysis/scripts/match_icd_codes.R
+++ analysis/scripts/match_icd_codes.R
@@ -6,71 +6,27 @@ library(readxl)
 code_list <- readRDS(here("analysis/files", "code_list.rds"))
 code_list_1 <- readRDS(here("analysis/files", "code_list_1.rds"))
 
-icd_range <- read_excel(here("analysis/files","icd_range_swe_lookup.xlsx"))
-
-icd_group_2<- read_excel(here("analysis/files","icd_groups_2.xlsx"), sheet = 3) %>% 
-  select(code,description) %>% 
-  mutate(group_2 = 1)
-
-
-icd_group <- read_excel(here("analysis/files","icd_10_codes.xlsx")) %>% 
-  mutate(group_1 = 1,
-         code = str_remove(code, "[.]"))
-
-a <- code_list %>% 
-  left_join(icd_range,
-            by = c('code' = 'code'))
+icd_range <- read_excel(here("analysis/files","icd_risk_group_lookup_final.xlsx"))
 
 
 b <- code_list_1 %>% 
   left_join(icd_range,
             by = c('code' = 'code')) %>% 
+  distinct(code, .keep_all = TRUE) %>% 
   arrange(desc(n))
 
+bb <- b %>% 
+  filter(source != 'icd_10_se_kap',
+         source != 'icd_10_se_kap_imputed',
+         source != 'icd_9_kapitel_conversion',
+         source != 'icd_10_se_kap',
+         code_type != 'kapitel')
 
-
-c <- a %>% 
-  left_join(icd_group_2,
-            by = "code") %>% 
-  left_join(icd_group,
-            by = c('code')
-  ) %>% 
-  filter(!is.na(text))
 
 
 
 saveRDS(c, here("analysis/files","icd_code_lookup.rds"))
 
+## dbparser ----
 
-
-library(icd)
-
-
-vignette("introduction", package = "icd")
-
-vignette("ranges", package = "icd")
-
-is_valid(code_list$code, short_code = TRUE)
-
-a <- explain_code(code_list$code, condense = TRUE) # finds 88 out of 2683 codes
-
-
-remotes::install_github("gforge/comorbidities.icd10")
-
-
-
-library(icdcoder)
-
-code_list <-  code_list %>% 
-  mutate()
-
-getICDRange("001","002", "icd9")
-getICDRange("A00","A03", "icd10")  
-
-
-
-library(decoder)
-
-a <- decode(code_list$code, "icd10se" )
-
-
+#dbparser package to parse the codes from the database https://search.r-project.org/CRAN/refmans/dbparser/html/drug_atc_codes.html
