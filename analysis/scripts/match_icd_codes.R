579c22123eff87b91a44d237068f4ab085e43eee
Nick Christie <30588830+n-christie@users.noreply.github.com>
2022-10-20 16:26:28 +0200

    progress on the lookup table

--- analysis/scripts/match_icd_codes.R
+++ analysis/scripts/match_icd_codes.R
@@ -9,13 +9,35 @@ code_list_1 <- readRDS(here("analysis/files", "code_list_1.rds"))
 icd_range <- read_excel(here("analysis/files","icd_risk_group_lookup_final.xlsx"))
 
 
-b <- code_list_1 %>% 
+unique_codes <- code_list_1 %>%
+  select(-n) %>% 
+  rbind(code_list) %>% 
+  distinct(code) %>% 
   left_join(icd_range,
             by = c('code' = 'code')) %>% 
   distinct(code, .keep_all = TRUE) %>% 
-  arrange(desc(n))
+  filter(!is.na(code),
+         code != "")
 
-bb <- b %>% 
+
+
+
+no_matched_codes <- code_list_1 %>%
+  select(-n) %>% 
+  rbind(code_list) %>% 
+  distinct(code) %>% 
+  anti_join(icd_range,
+            by = c('code' = 'code')) %>% 
+  distinct(code, .keep_all = TRUE) %>% 
+  filter(!is.na(code),
+         code != "") %>% 
+  arrange(
+    
+  )
+
+
+
+codes_to_link <- unique_codes %>% 
   filter(source != 'icd_10_se_kap',
          source != 'icd_10_se_kap_imputed',
          source != 'icd_9_kapitel_conversion',
