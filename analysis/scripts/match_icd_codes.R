b8bb99a421ae4de77c8ea942a2cb9a249a9e98c9
Nick Christie <30588830+n-christie@users.noreply.github.com>
2022-10-27 14:35:43 +0200

    made matching table to work on with giedre

--- analysis/scripts/match_icd_codes.R
+++ analysis/scripts/match_icd_codes.R
@@ -1,40 +1,41 @@
 library(here)
 library(tidyverse)
 library(readxl)
+library(openxlsx)
 
 
-code_list <- readRDS(here("analysis/files", "code_list.rds"))
-code_list_1 <- readRDS(here("analysis/files", "code_list_1.rds"))
+ code_list <- readRDS(here("analysis/files", "unique_icd.rds"))
+# code_list_1 <- readRDS(here("analysis/files", "code_list_1.rds"))
 
-icd_range <- read_excel(here("analysis/files","icd_risk_group_lookup_final.xlsx"))
+icd_range <- read_excel(here("analysis/files","icd_risk_group_lookup_final.xlsx")) %>% 
+  drop_na(code)
 
-
-unique_codes <- code_list_1 %>%
-  select(-n) %>% 
-  rbind(code_list) %>% 
-  distinct(code) %>% 
-  left_join(icd_range,
-            by = c('code' = 'code')) %>% 
-  distinct(code, .keep_all = TRUE) %>% 
-  filter(!is.na(code),
-         code != "")
+# atc_lookup <- read_csv("analysis/files/ATC.csv") %>% 
+#   select(code = CUI,
+#          "description" = "Preferred Label" ) %>% 
+#   mutate(code_type = "atc")
 
 
+## there are 3297 unique codes from slutvård dataset
+## matching on icd description data - 1689 codes remain unmatched
+## this apperars to be due to atc codes in the sample. 
 
 
-no_matched_codes <- code_list_1 %>%
-  select(-n) %>% 
-  rbind(code_list) %>% 
-  distinct(code) %>% 
+
+no_matched_codes <- code_list %>%
   anti_join(icd_range,
-            by = c('code' = 'code')) %>% 
-  distinct(code, .keep_all = TRUE) %>% 
-  filter(!is.na(code),
-         code != "") %>% 
-  arrange(
-    
-  )
+            by = c('value' = 'code')) %>% 
+  arrange()
 
+matched_codes <- code_list %>%
+  left_join(icd_range,
+            by = c('value' = 'code')) %>% 
+  distinct(value, .keep_all = TRUE) %>% 
+  drop_na(label_swe) %>% 
+  arrange()
+
+
+ 
 
 
 codes_to_link <- unique_codes %>% 
@@ -44,10 +45,45 @@ codes_to_link <- unique_codes %>%
          source != 'icd_10_se_kap',
          code_type != 'kapitel')
 
+## Risk groups
 
+risk_groups <- read_excel(here("analysis/files","ICD for RELOC-AGE Registers ICD10 and ICD9 v2.xlsx")) %>% 
+  rename(value = "code",
+         #code_category = "Categorization",
+         #disease_category = "Immunodeficiency* or organ transplant"
+  )
+
+
+
+matched_risk_group <- matched_codes %>%
+  remove_empty('cols') %>% 
+  left_join(risk_groups,
+            by = c('value' )) %>% 
+  distinct(value,.keep_all = TRUE) %>%
+  remove_empty('cols')
+  select(-"influenza_risk"  ,
+         -"stroke_risk.x" ,
+         )
+  arrange()
+
+
+no_matched_risk_group <- risk_groups %>%
+  anti_join(code_list,
+            by = c('value' )) %>% 
+  #drop_na(code_category) %>% 
+  distinct(value,.keep_all = TRUE) %>% 
+  arrange()
+
+write.xlsx(no_matched_risk_group, here("analysis/files/risk_groups","unmatched_group_codes.xlsx") )
+write.xlsx(matched_risk_group, here("analysis/files/risk_groups","matched_group_codes.xlsx") )
 
 
 saveRDS(c, here("analysis/files","icd_code_lookup.rds"))
+
+
+
+
+
 
 ## dbparser ----
 
